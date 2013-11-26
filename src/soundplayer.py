b35c2d561c95f3402ac40dfa7318ac48cfd456b8
Daniel Andres <daniel.lopez@student.hs-rm.de>
2013-11-26 09:40:20 +0100

    more on soundplayer

--- src/soundplayer.py
+++ src/soundplayer.py
@@ -1,29 +1,19 @@
 import pyaudio
-## To use wavebender checkout this repo: https://github.com/zacharydenton/wavebender.git and install it with python setup.py
+# # To use wavebender checkout this repo: https://github.com/zacharydenton/wavebender.git and install it with python setup.py
 from wavebender import *
 
-def playsound(frequency, duration):
-    PyAudio = pyaudio.PyAudio
-
-    RATE = 44100
-    p = PyAudio()
-      
-    stream = p.open(format=
-                    p.get_format_from_width(2),
-                    channels=1,
-                    rate=RATE,
-                    output=True)
-    channels = ((sine_wave(frequency),),)
-    samples = compute_samples(channels, RATE * duration * 1)
+def playsound(frequency=440.0, amplitude=0.5, framerate=44100, duration=30):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(2), channels=1, rate=framerate, output=True)
+    channels = ((sine_wave(frequency, amplitude=amplitude, framerate=framerate),),)
+    samples = compute_samples(channels, framerate * duration * 1)
     write_wavefile(stream, samples)
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-
-
 def main():
-    playsound(19000.0, 30)
+    playsound(21000.0, framerate=48100, duration=30)
     pass
 
 if __name__ == '__main__':
