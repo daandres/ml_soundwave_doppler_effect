a07b0cd603e030b8156363507957e7b8b9be1264
ppasl002 <paul@ppasler.de>
2013-12-06 15:57:37 +0100

    Player spielt unendlich lange AttributeError behoben - zwar nicht sauber, aber geht

--- src/soundplayer.py
+++ src/soundplayer.py
@@ -1,26 +1,32 @@
 import pyaudio
 # # To use wavebender checkout this repo: https://github.com/zacharydenton/wavebender.git and install it with python setup.py
 import wavebender as wb
-
+from myStream import MyStream
 class Sound:
     
     def __init__(self):
         self.audioDev = pyaudio.PyAudio()
         self.audioStream = None
+        self.play = True
     
     def startPlaying(self, frequency=440.0, amplitude=0.5, framerate=48100, duration=30):
         # create stream
-        self.audioStream = self.audioDev.open(format=self.audioDev.get_format_from_width(2), 
-                                              channels=1, rate=framerate, output=True)
         channels = ((wb.sine_wave(frequency, amplitude=amplitude, framerate=framerate),),)
-        samples = wb.compute_samples(channels, framerate * duration * 1)
-        wb.write_wavefile(self.audioStream, samples)
-        
-    
+               
+        while True:
+            try:
+                samples = wb.compute_samples(channels, framerate * duration * 1)
+                self.audioStream = MyStream(self.audioDev.open(format=self.audioDev.get_format_from_width(2), channels=1, rate=framerate, output=True))
+                wb.write_wavefile(self.audioStream, samples)
+            except AttributeError:
+                pass
+            if self.play == False:
+                self.audioStream.stopIt()
+                self.audioDev.terminate()
+                break
+
     def stopPlaying(self):
-        self.audioStream.stop_stream()
-        self.audioStream.close()
-        self.audioDev.terminate()   
+        self.play = False
 
 if __name__ == '__main__':
     s = Sound()
