b2edc911b1808d6b6155a7046bcaae72817fa6e1
Daniel Andres <daniel.lopez@student.hs-rm.de>
2014-01-14 11:52:32 +0100

    svm with benny

--- src/classifier/svm/svm.py
+++ src/classifier/svm/svm.py
@@ -12,18 +12,20 @@ from sklearn.metrics import accuracy_score
 from gestureFileIO import GestureFileIO
 
 class SVM(IClassifier):
-    
+
     def __init__(self, recorder=None, config=None, relative=""):
         self.avg = self.getAverage()
         self.classifier = self.load("classifier/svm/svm_trained.pkl")
         self.datalist = []
         self.datanum = 0
-    
+        self.nClasses = 7
+        self.loadData()
+
     def getAverage(self):
         g = GestureFileIO()
         avg = g.getAvgFrequency()
         return avg
-    
+
     def load(self, filename=""):
         return joblib.load(filename)
 
@@ -52,13 +54,41 @@ class SVM(IClassifier):
         pass
 
     def startValidation(self):
-        pass
+        confmat = np.zeros((self.nClasses, self.nClasses))
+#         print("target-out")
+        for c in range(self.data.shape[0]):
+            out = None
+            target = c
+            j = 0
+            for geste in self.data[c]:
+                out = self.classifier.predict(geste)
+                confmat[target][out[0]] += 1
+#             print("out:\t" + str(out) + "\ttarget:\t" + str(target))
+#             print(str(np.argmax(target)) + "-" + str(np.argmax(out)))
+        sumWrong = 0
+        sumAll = 0
+        for i in range(self.nClasses):
+            for j in range(self.nClasses):
+                if i != j:
+                    sumWrong += confmat[i][j]
+                sumAll += confmat[i][j]
+        error = sumWrong / sumAll
+        print(confmat)
+        print("error: " + str(100. * error) + "%")
 
     def save(self, filename=""):
         pass
 
     def loadData(self, filename=""):
-        pass
+        g = GestureFileIO()
+        data = [0] * self.nClasses
+        for i in range(self.nClasses):
+            datum = g.getGesture3DDiffAvg(i, [])
+            print("data " + str(i) + " loaded shape: " + str(np.shape(datum)))
+            data[i] = datum.reshape(datum.shape[0], datum.shape[1] * datum.shape[2])
+        data = np.asarray(data)
+        self.data = data
+
 
     def saveData(self, filename=""):
         pass
--- src/classifier/svm/svm_original.py
+++ src/classifier/svm/svm_original.py
@@ -12,130 +12,130 @@ from sklearn.externals import joblib
 
 
 def train_svm(X_train, Y_train, X_test, Y_test, c, gamma, method, cv, kernel):
-    
+
     ''' Initialise the model with best model parameters reported at '''
     ''' http://peekaboo-vision.blogspot.co.uk/2010/09/mnist-for-ever.html '''
     print "...initialising svm"
     classifier = svm.SVC(kernel=kernel, C=c, gamma=gamma)
-    
+
     ''' Train and validate the classifier '''
-    if method == "fit":   
+    if method == "fit":
         print "...performing normal fitting"
-        classifier.fit(X_train, Y_train)  
+        classifier.fit(X_train, Y_train)
         Y_pred = classifier.predict(X_test)
         accuracy = accuracy_score(Y_test, Y_pred)
-        result = str(accuracy*100)[:5]+"%"
-        
+        result = str(accuracy * 100)[:5] + "%"
+
     if method == "cross":
         print "...performing cross-validation"
         accuracy = cross_validation.cross_val_score(classifier, X_train, Y_train, cv=cv)
-        result = str(numpy.average(accuracy)*100)[:5]+"%"
-    
+        result = str(numpy.average(accuracy) * 100)[:5] + "%"
+
     joblib.dump(classifier, 'svm_trained.pkl', compress=9)
-    
+
     print "\naccuracy:\t", result
-    
-def normalise(arr,nn_avg):
+
+def normalise(arr, nn_avg):
     ''' normalise each frame '''
     for d in range(len(arr)):
         for dd in range(len(arr[d])):
-            arr[d][dd] = (arr[d][dd]/numpy.amax(arr[d][dd]))-nn_avg
+            arr[d][dd] = (arr[d][dd] / numpy.amax(arr[d][dd])) - nn_avg
     return arr
 
-    
+
 def preprocess():
     names = ["Alex", "Annalena", "Benjamin", "Daniel", "Frank"]
-    #names = ["Alex", "Annalena", "Daniel", "Benjamin"]
+    # names = ["Alex", "Annalena", "Daniel", "Benjamin"]
     path = "../../../gestures/"
     threshold = 0.1
-    
+
     ''' load and reshape textfile with 18.5khz frequency data '''
-    n = numpy.loadtxt(path+"Benjamin/gesture_6/1389637026.txt",delimiter=",")
-    n = n.reshape(n.shape[0],32,n.shape[1]/32) #recordingframes
-    nn = normalise(n,0)
+    n = numpy.loadtxt(path + "Benjamin/gesture_6/1389637026.txt", delimiter=",")
+    n = n.reshape(n.shape[0], 32, n.shape[1] / 32)  # recordingframes
+    nn = normalise(n, 0)
     ''' create average of 18.5khz frequency data '''
     nn_avg = numpy.mean(nn, axis=1)
     nn_avg = numpy.mean(nn_avg, axis=0)
-    #print nn_avg.shape
-    
-    
+    # print nn_avg.shape
+
+
     gestures = []
     targets = []
-    for gesturenumber in range(6):
-        
+    for gesturenumber in range(7):
+
         for name in names:
-            dirf = os.listdir(path+name+"/gesture_"+str(gesturenumber))
-            #firsttextfile = dirf[0]
-            
+            dirf = os.listdir(path + name + "/gesture_" + str(gesturenumber))
+            # firsttextfile = dirf[0]
+
             for txtfile in dirf:
                 ''' load and reshape textfile with gesture data '''
-                g = numpy.loadtxt(path+name+"/gesture_"+str(gesturenumber)+"/"+txtfile,delimiter=",")
-                g = g.reshape(g.shape[0],32,g.shape[1]/32) #recordingframes
+                g = numpy.loadtxt(path + name + "/gesture_" + str(gesturenumber) + "/" + txtfile, delimiter=",")
+                g = g.reshape(g.shape[0], 32, g.shape[1] / 32)  # recordingframes
                 print name, gesturenumber, "\t\t", g.shape, txtfile
-                gn = normalise(g,nn_avg)
-                
-                gn = gn.reshape(gn.shape[0],gn.shape[1]*gn.shape[2])
-                
+                gn = normalise(g, nn_avg)
+
+                gn = gn.reshape(gn.shape[0], gn.shape[1] * gn.shape[2])
+
                 for i in range(gn.shape[0]):
                     gestures.append(gn[i])
                     targets.append(gesturenumber)
-        
+
     X_data = numpy.array(gestures)
     X_targets = numpy.array(targets)
-    
+
     print "X_data", "\t\t", X_data.shape
     print "X_targets", "\t", X_targets.shape
-    
+
     print X_targets
-    
+
     return X_data, X_targets, nn_avg
-    
+
     #===========================================================================
     # ''' normalise data '''
     # nn = normalise(n)
     # gn = normalise(g)
-    # 
+    #
     # gn = gn.reshape(gn.shape[0],gn.shape[1]*gn.shape[2])
     # print gn.shape
     # return
-    # 
+    #
     # ''' create average of 18.5khz frequqncy data '''
     # nn_avg = numpy.mean(nn, axis=1)
     # nn_avg = numpy.mean(nn_avg, axis=0)
-    # 
+    #
     # return nn_avg, gn
     #===========================================================================
-    
-    
+
+
 def test(nn_avg):
     path = "../../../gestures/Alex/"
-    
-    n = numpy.loadtxt(path+"gesture_0/ZimmerLeise2R.txt",delimiter=",")
-    n = n.reshape(n.shape[0],32,n.shape[1]/32) #recordingframes
-    nn = normalise(n,nn_avg)
-    nn = nn.reshape(nn.shape[0],nn.shape[1]*nn.shape[2])
-    
+
+    n = numpy.loadtxt(path + "gesture_0/ZimmerLeise2R.txt", delimiter=",")
+    n = n.reshape(n.shape[0], 32, n.shape[1] / 32)  # recordingframes
+    nn = normalise(n, nn_avg)
+    nn = nn.reshape(nn.shape[0], nn.shape[1] * nn.shape[2])
+
     Y_data = nn[10:]
     Y_targets = numpy.zeros(Y_data.shape[0], dtype=int)
-    
+
     print "Y_data", "\t\t", Y_data.shape
     print "Y_targets", "\t", Y_targets.shape
-    
+
     return Y_data, Y_targets
-    
+
 
 
 if __name__ == "__main__":
 
     X_data, X_targets, nn_avg = preprocess()
-    c = 10000
+    c = 1000000
     gamma = 0
     cv = 3
     method = "fit"
-    kernel = "rbf" 
-    
-    Y_data,Y_targets = test(nn_avg)
-    
+    kernel = "rbf"
+
+    Y_data, Y_targets = test(nn_avg)
+
     train_svm(X_data, X_targets, Y_data, Y_targets, c, gamma, method, cv, kernel)
 
 
