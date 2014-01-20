d3fa459991dc4c077a4c6b2236cc8cdb135ad4b6
Benny <Ben.Weisser@gmx.de>
2014-01-20 20:11:40 +0100

    svm++

--- src/classifier/svm/svm.py
+++ src/classifier/svm/svm.py
@@ -12,18 +12,18 @@ from sklearn.metrics import accuracy_score
 from gestureFileIO import GestureFileIO
 from sklearn import svm
 from sklearn import cross_validation
-
+from collections import Counter
 from scipy import stats as stats
 
 class SVM(IClassifier):
 
     def __init__(self, recorder=None, config=None, relative=""):
-        self.avg = self.getAverage()
+        #self.avg = self.getAverage()
         self.classifier = self.load("classifier/svm/svm_trained.pkl")
         self.datalist = []
         self.datanum = 0
         self.nClasses = 7
-        self.data, self.targets = self.loadData()
+        self.data, self.targets, self.avg = self.loadData()
         self.kernel = "rbf"
         self.c = 1000000
         self.gamma = 1
@@ -36,6 +36,9 @@ class SVM(IClassifier):
         self.predHistSize = 6
         self.predHistHalfUpper = 4
         self.predHistory = self.createArraySix(self.predHistSize,)
+        
+        self.foundgesture = False
+        self.foundgestureindex = 0
 
     def createArraySix(self, dim):
         array = np.zeros((dim,))
@@ -53,6 +56,43 @@ class SVM(IClassifier):
 
 
     def classify(self, data):
+        normalizedData = data / np.amax(data)
+        diffAvgData = normalizedData - self.avg
+        
+        self.datalist.append(diffAvgData)
+        self.datanum += 1
+        
+        #=======================================================================
+        # T = diffAvgData - self.avg
+        # diff = np.max(np.abs(T))
+        # if diff < 1:
+        #     self.foundgestureindex = self.datanum
+        #=======================================================================
+
+    
+        plus = 8
+        if(self.datanum == 32+plus):
+            preds = []
+            for i in range(plus):
+                g = np.asarray(self.datalist[i:32+i]).reshape(2048,)
+                Y_pred = self.classifier.predict(g)[0]
+                preds.append(Y_pred)
+            
+            c = Counter(preds)
+            
+            #if np.unique(np.asarray(preds)).size != len(preds): # and (self.datanum-32-plus) < self.foundgestureindex:
+            if max(set(preds), key=preds.count) != 6:
+                #print preds
+                print "unique classes:\t\t",len(list(set(preds)))
+                print "count of classes:\t",c
+                print "predicted class:\t",max(set(preds), key=preds.count)
+                print "\n",60*"=","\n"
+            self.datalist = []
+            self.datanum = 0
+            #print "\t\t\t new gesture"
+            
+
+    def classifyold(self, data):
         normalizedData = data / np.amax(data)
         diffAvgData = normalizedData - self.avg
 
@@ -119,7 +159,54 @@ class SVM(IClassifier):
     def save(self, filename=""):
         pass
 
+    
     def loadData(self, filename=""):
+        
+        def normalise(arr, nn_avg):
+            ''' normalise each frame '''
+            for d in range(len(arr)):
+                for dd in range(len(arr[d])):
+                    arr[d][dd] = (arr[d][dd] / np.amax(arr[d][dd])) - nn_avg
+            return arr
+        
+        names = ["Benjamin"]
+        path = "../gestures/"
+
+        ''' load and reshape textfile with 18.5khz frequency data '''
+        n = np.loadtxt(path + "Benjamin/gesture_6/1390243484.txt", delimiter=",")
+        n = n.reshape(n.shape[0], 32, n.shape[1] / 32)  # recordingframes
+        nn = normalise(n, 0)
+        nn_avg = np.mean(nn, axis=1)
+        nn_avg = np.mean(nn_avg, axis=0)
+
+    
+        gestures = []
+        targets = []
+        for gesturenumber in range(7):
+            for name in names:
+                dirf = os.listdir(path + name + "/gesture_" + str(gesturenumber))
+
+                for txtfile in dirf:
+                    ''' load and reshape textfile with gesture data '''
+                    g = np.loadtxt(path + name + "/gesture_" + str(gesturenumber) + "/" + txtfile, delimiter=",")
+                    g = g.reshape(g.shape[0], 32, g.shape[1] / 32)  # recordingframes
+                    print name, gesturenumber, "\t\t", g.shape, txtfile
+                    gn = normalise(g, nn_avg)
+    
+                    gn = gn.reshape(gn.shape[0], gn.shape[1] * gn.shape[2])
+    
+                    for i in range(gn.shape[0]):
+                        gestures.append(gn[i])
+                        targets.append(gesturenumber)
+    
+        data = np.array(gestures)
+        targets = np.array(targets)
+        
+        print data.shape, targets.shape
+        
+        return data, targets, nn_avg
+
+    def loadData_old(self, filename=""):
         g = GestureFileIO()
         X = []
         Y = []
