a43c69ea9741e863234617df6b75b59074841595
sried004 <sebastian.b.rieder@student.hs-rm.de>
2014-01-21 08:37:28 +0100

    View Anpassung vom Robert

--- src/view/ui_bob_visualizer_2.py
+++ src/view/ui_bob_visualizer_2.py
@@ -9,30 +9,30 @@ from threading import Thread
 from properties.config import ConfigProvider
 import time
 
-# from classifier.k_means.kMeans import kmeans
+#from classifier.k_means.kMeans import kmeans
 
 import copy
 
-# from enum import Enum
+from enum import Enum
 class CallbackMessage():
     bufferFilled = 1
-    progress = 2
-
-
-
+    progress = 2 
+    
+    
+    
 
 
 class ViewUIBob:
     def __init__(self, recorder=None, applicationClose=None):
-    # def __init__(self, applicationClose=None):
-
+    #def __init__(self, applicationClose=None):
+        
         self.cm = CallbackMessage()
         self.callbackButton = None
         self.calibrationBarValue = 0
         self.recordsBarValue = 0
-
+        
         self.kmeans = None
-
+        
         self.plot_id = 1
         self.initCalArray = []
         self.calibMean = []
@@ -44,66 +44,66 @@ class ViewUIBob:
         self.record = False
         self.showRecords = False
         self.fileName = None
-
+        
         self.calSampCount = 0
         self.calibSampleList = []
-
+        
         self.learnArray = []
-
+        
         self.gestureFrameCount = 32
         self.calibratetSignal = False
         self.plotBinaryCurve = False
-
-        self.xPoints = np.array([ 17742.1875, 17765.625, 17789.0625, 17812.5  , 17835.9375, 17859.375,
-                                  17882.8125, 17906.25 , 17929.6875, 17953.125, 17976.5625, 18000.   ,
-                                  18023.4375, 18046.875, 18070.3125, 18093.75 , 18117.1875, 18140.625,
-                                  18164.0625, 18187.5  , 18210.9375, 18234.375, 18257.8125, 18281.25 ,
-                                  18304.6875, 18328.125, 18351.5625, 18375.   , 18398.4375, 18421.875,
-                                  18445.3125, 18468.75 , 18492.1875, 18515.625, 18539.0625, 18562.5  ,
-                                  18585.9375, 18609.375, 18632.8125, 18656.25 , 18679.6875, 18703.125,
-                                  18726.5625, 18750.   , 18773.4375, 18796.875, 18820.3125, 18843.75 ,
-                                  18867.1875, 18890.625, 18914.0625, 18937.5  , 18960.9375, 18984.375,
-                                  19007.8125, 19031.25 , 19054.6875, 19078.125, 19101.5625, 19125.   ,
-                                  19148.4375, 19171.875, 19195.3125, 19218.75  ])
-
+        
+        self.xPoints = np.array([ 17742.1875, 17765.625,   17789.0625,  17812.5  ,   17835.9375,  17859.375,
+                                  17882.8125, 17906.25 ,   17929.6875,  17953.125,   17976.5625,  18000.   ,
+                                  18023.4375, 18046.875,   18070.3125,  18093.75 ,   18117.1875,  18140.625,
+                                  18164.0625, 18187.5  ,   18210.9375,  18234.375,   18257.8125,  18281.25 ,
+                                  18304.6875, 18328.125,   18351.5625,  18375.   ,   18398.4375,  18421.875,
+                                  18445.3125, 18468.75 ,   18492.1875,  18515.625,   18539.0625,  18562.5  ,
+                                  18585.9375, 18609.375,   18632.8125,  18656.25 ,   18679.6875,  18703.125,
+                                  18726.5625, 18750.   ,   18773.4375,  18796.875,   18820.3125,  18843.75 ,
+                                  18867.1875, 18890.625,   18914.0625,  18937.5  ,   18960.9375,  18984.375,
+                                  19007.8125, 19031.25 ,   19054.6875,  19078.125,   19101.5625,  19125.   ,
+                                  19148.4375, 19171.875,   19195.3125,  19218.75  ])
+        
         # application
-
+        
         self.app = None
+        
 
-
-
+       
         self.uiplot_tab1, self.curve_tab1 = None, None
         self.curve_2_tab1 = None
+       
+        self.curve_1 , self.curve_2 , self.curve_3 , self.curve_4 , self.curve_5 , self.curve_6 , self.curve_7 ,self.curve_8 = None,None,None,None,None,None,None,None
+        self.curve_9 ,self.curve_10 ,self.curve_11 ,self.curve_12 ,self.curve_13 ,self.curve_14 , self.curve_15 , self.curve_16 = None,None,None,None,None,None,None,None
+        self.curve_17 , self.curve_18 , self.curve_19 , self.curve_20 , self.curve_21 , self.curve_22 , self.curve_23 , self.curve_24 = None,None,None,None,None,None,None,None
+        self.curve_25 , self.curve_26 ,self.curve_27 ,self.curve_28 ,self.curve_29 ,self.curve_30 ,self.curve_31 ,self.curve_32 = None,None,None,None,None,None,None,None         
+        
 
-        self.curve_1 , self.curve_2 , self.curve_3 , self.curve_4 , self.curve_5 , self.curve_6 , self.curve_7 , self.curve_8 = None, None, None, None, None, None, None, None
-        self.curve_9 , self.curve_10 , self.curve_11 , self.curve_12 , self.curve_13 , self.curve_14 , self.curve_15 , self.curve_16 = None, None, None, None, None, None, None, None
-        self.curve_17 , self.curve_18 , self.curve_19 , self.curve_20 , self.curve_21 , self.curve_22 , self.curve_23 , self.curve_24 = None, None, None, None, None, None, None, None
-        self.curve_25 , self.curve_26 , self.curve_27 , self.curve_28 , self.curve_29 , self.curve_30 , self.curve_31 , self.curve_32 = None, None, None, None, None, None, None, None
-
-
-
-
-
+    
+   
+        
     def showRecord(self):
         self.uiplot.skipFrames_sb.setStyleSheet('QSpinBox {color: green}')
         self.showRecords = True
         self.framesToSkip = self.uiplot.skipFrames_sb.value()
         showAny = self.uiplot.showAnyX_sb.value()
-
-        if ((self.framesToSkip + 32) > len(self.recordArray)):
+        
+        if ((self.framesToSkip+32) > len(self.recordArray)):
             self.framesToSkip = 0
             self.uiplot.skipFrames_sb.setStyleSheet('QSpinBox {color: red}')
-        for i in range (1, 33):
+        for i in range (1,33):
             print i
-            qwtPlot = getattr(self.uiplot , 'qwtPlot_%d' % i)
-            curr = getattr(self , 'curve_%d' % i)
-
-            curr.setData(self.xPoints, self.recordArray[i * showAny + self.framesToSkip])
+            qwtPlot = getattr(self.uiplot ,'qwtPlot_%d' %i)
+            curr = getattr(self ,'curve_%d' %i)
+                        
+            curr.setData(self.xPoints, self.recordArray[i*showAny + self.framesToSkip])
             qwtPlot.replot()
-
+    
     def showPlot(self):
         self.showRecords = False
-
+        
     def openFile(self):
         print QtCore.QDir.currentPath()
         self.fileName = QtGui.QFileDialog.getOpenFileNames(self.win_plot, "Open ",
@@ -112,87 +112,87 @@ class ViewUIBob:
 
     def fixpath(self, path):
         return path.replace('\\', '/')
-
+    
     def getArrayFromFile(self, path):
         arr = np.loadtxt(path, delimiter=",")
-
+        
         if len(arr.shape) == 1:
-            frames = arr.shape / 64
+            frames = arr.shape/64
             arr = arr.reshape((1, frames, 64))
             self.learnArray = np.asarray(arr)
         else:
-            frames = arr.shape[1] / 64
-            arr = arr.reshape(arr.shape[0], frames, 64)
+            frames = arr.shape[1]/64
+            arr = arr.reshape(arr.shape[0],frames,64)
             self.learnArray = np.asarray(arr)
-
+             
         print self.learnArray.shape
-
+   
     def loadLearnArray(self):
         self.uiplot.arrayIdxLF_sb.setStyleSheet('QSpinBox {color: green}')
         self.showRecords = True
         rAIdx = self.uiplot.arrayIdxLF_sb.value()
         skipFrames = self.uiplot.skipFramesLA_sb.value()
-
-        if (rAIdx > self.learnArray.shape[0] - 1):
+        
+        if (rAIdx > self.learnArray.shape[0]-1):
             rAIdx = 0
             self.uiplot.arrayIdxLF_sb.setStyleSheet('QSpinBox {color: red}')
             rAIdx = self.uiplot.arrayIdxLF_sb.setValue(0)
-        for i in range (0, 32):
-            ii = i + 1
-            qwtPlot = getattr(self.uiplot , 'qwtPlot_%d' % ii)
-            curr = getattr(self , 'curve_%d' % ii)
-
-            ys = self.learnArray[rAIdx][i + skipFrames]
-
+        for i in range (0,32):
+            ii = i+1
+            qwtPlot = getattr(self.uiplot ,'qwtPlot_%d' %ii)
+            curr = getattr(self ,'curve_%d' %ii)
+            
+            ys = self.learnArray[rAIdx][i+skipFrames]
+            
             if self.calibratetSignal:
                 ys = ys - self.calibMean
-
+            
             if self.plotBinaryCurve:
-                # dVec = np.asarray(ys)
+                #dVec = np.asarray(ys)
                 lowValY = self.uiplot.binaryPlot_dsb.value()
                 lvidx = ys < lowValY  # Where values are low
-                ys[lvidx] = 0.0
+                ys[lvidx] = 0.0 
                 hvidx = ys > lowValY  # Where values are height
-                ys[hvidx] = 500.0
-
+                ys[hvidx] = 500.0 
+                        
             curr.setData(self.xPoints, ys)
             qwtPlot.replot()
-
+     
     def showRecordBinary(self):
         if self.showRecords:
             self.showRecordByRecorder()
-
+       
     def initKMeans(self):
         k = self.uiplot.kNumber_sb.value()
         maxIteration = self.uiplot.maxIteration_sb.value()
-        # self.k
-
+        #self.k
+        
     def learnKMeans(self):
         print 'learnMeans'
-
+        
     def checkKMeans(self):
         print 'checkMeans'
-
-
+        
+    
     def callback(self, message, num):
-
+        
         if message == 0:
             print '0'
-
+            
         elif message == 1:
             self.recordsBarValue = 100
         elif message == 2:
             self.calibrationBarValue = num
             self.uiplot.calibration_bt.setStyleSheet('QPushButton  {color: green} ')
-
+              
     def bindButtons(self):
 
 
 
 
-        # self.uiplot.record_bt.clicked.connect(self.recordTest)
+        #self.uiplot.record_bt.clicked.connect(self.recordTest)
 
-        # self.uiplot.showRecord_bt.clicked.connect(self.showRecord)
+        #self.uiplot.showRecord_bt.clicked.connect(self.showRecord)
 
         self.uiplot.showPlot_bt.clicked.connect(self.showPlot)
 
@@ -200,17 +200,17 @@ class ViewUIBob:
         self.uiplot.loadLearnArray_bt.clicked.connect(self.loadLearnArray)
         self.uiplot.arrayIdxLF_sb.valueChanged.connect(self.loadLearnArray)
         self.uiplot.skipFramesLA_sb.valueChanged.connect(self.loadLearnArray)
-
+        
         self.uiplot.binaryPlot_dsb.valueChanged.connect(self.showRecordBinary)
-
+        
         '''kmeans'''
         self.uiplot.initKMeans_bt.clicked.connect(self.initKMeans)
         self.uiplot.learnKMeans_bt.clicked.connect(self.learnKMeans)
         self.uiplot.checkKMeans_bt.clicked.connect(self.checkKMeans)
-
-
-
-
+    
+    
+        
+        
     def startNewThread(self):
         self.t = Thread(name="ControlGuiBob", target=self.start, args=())
         self.t.start()
@@ -220,7 +220,7 @@ class ViewUIBob:
         if self.t is not None:
             return self.t.is_alive()
         return False
-
+   
     def start(self):
         # application
         self.app = QtGui.QApplication(sys.argv)
@@ -228,53 +228,53 @@ class ViewUIBob:
         self.win_plot = ui_bob.QtGui.QMainWindow()
         self.uiplot = ui_bob.Ui_MainWindow()
         self.uiplot.setupUi(self.win_plot)
-
+        
         self.curve_tab1 = Qwt.QwtPlotCurve()
         self.curve_2_tab1 = Qwt.QwtPlotCurve()
-
+        
         self.curve_1, self.curve_2, self.curve_3, self.curve_4, self.curve_5, self.curve_6, self.curve_7, self.curve_8 = Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve()
         self.curve_9, self.curve_10, self.curve_11, self.curve_12, self.curve_13, self.curve_14, self.curve_15, self.curve_16 = Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve()
         self.curve_17, self.curve_18, self.curve_19, self.curve_20, self.curve_21, self.curve_22, self.curve_23, self.curve_24 = Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve()
         self.curve_25, self.curve_26, self.curve_27, self.curve_28, self.curve_29, self.curve_30, self.curve_31, self.curve_32 = Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve(), Qwt.QwtPlotCurve()
-
-        self.curve_tab1.attach(self.uiplot.qwtPlot_tab1)
-        self.curve_2_tab1.attach(self.uiplot.qwtPlot_tab1)
-        self.uiplot.qwtPlot_tab1.setAxisScale(self.uiplot.qwtPlot_tab1.yLeft, 0, 1 * 1000)
-        self.uiplot.qwtPlot_tab1.setAxisScale(self.uiplot.qwtPlot_tab1.xBottom, 18117.1875, 18843.75)
+        
 
 
 
+
+       
+      
+        
         plot = self.uiplot
 
-        for i in range(1, 33):
+        for i in range(1,33):
 
-            qwtPlot = getattr(plot , 'qwtPlot_%d' % i)
-            curr = getattr(self , 'curve_%d' % i)
-            # curr.setPen({color: green} )
-            # curr.setStyle(Qwt.QwtPlotCurve.Dots)
-            curr.attach(qwtPlot)
+            qwtPlot = getattr(plot ,'qwtPlot_%d' %i)
+            curr = getattr(self ,'curve_%d' %i)
+            #curr.setPen({color: green} )
+            #curr.setStyle(Qwt.QwtPlotCurve.Dots)
+            curr.attach(qwtPlot)    
             qwtPlot.setAxisScale(qwtPlot.yLeft, -100, 1 * 1000)
-
-
+        
+        
         self.uiplot.timer = QtCore.QTimer()
         self.uiplot.timer.start(2)
 
-        # self.win_plot.connect(self.uiplot.timer, QtCore.SIGNAL('timeout()'), self.plotSignal)
-
-
-
+        #self.win_plot.connect(self.uiplot.timer, QtCore.SIGNAL('timeout()'), self.plotSignal)
+        
+        
+        
         self.bindButtons()
-
-
-
+        
+    
+            
 
         # ## DISPLAY WINDOWS
         self.win_plot.show()
-
+       
         code = self.app.exec_()
         self.applicationClose(code)
-
-
+       
+           
 #         sys.exit(code)
 #         self.recorder.close()
 
@@ -287,3 +287,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
         exitApp()
+    
