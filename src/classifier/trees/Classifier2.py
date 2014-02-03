5b0389eb2b68a68978634a35c230f6437741e8e3
Annalena <a.gutheil@yahoo.de>
2014-02-04 00:05:58 +0100

    gestures, feature extraction

--- src/classifier/trees/Classifier2.py
+++ src/classifier/trees/Classifier2.py
@@ -9,28 +9,43 @@ from classifier.trees.GestureModel import GestureModel
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+#from sklearn.ensemble import BaggingClassifier
 import numpy
 
 
 #gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_0/1388424714_zimmer_left.txt")
 
 #gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_1/1387647578_zimmer_left.txt")
-gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_2/1387660041_fernsehen.txt")
-gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_3/1387647860_zimmer_left.txt")
-gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_4/1387647860_zimmer_left.txt")
-gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_6/gesture_6_zimmer_1.txt")
 
+#gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_2/1387660041_fernsehen.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_3/1387647860_zimmer_left.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_4/1387647860_zimmer_left.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_6/gesture_6_zimmer_1.txt")
 
+#gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_2/1387647860_zimmer.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_2/1387660041_fernsehen.txt")
+#gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Annalena/gesture_3/1391439011.txt")
+#gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Annalena/gesture_3/1391439281.txt")
+#gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Annalena/gesture_4/1391439536.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Annalena/gesture_4/1391439659.txt")
+gestures = classifier.trees.ProcessData.getTestData("../../../gestures/Annalena/gesture_0/1391435081.txt")
+gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Annalena/gesture_0/1391435669.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_6/gesture_6_zimmer_1.txt")
+#gestures += classifier.trees.ProcessData.getTestData("../../../gestures/Daniel/gesture_6/gesture_6_zimmer_3.txt")
+
+#for i in range(150,200):
+counter = 0
 for i in range(len(gestures)):
     featureVector = []
     relative = gestures[i].smoothRelative(gestures[i].bins_left_filtered, gestures[i].bins_right_filtered, 2)
-    gestures[i].bins_left_filtered, gestures[i].bins_right_filtered = gestures[i].smoothToMostCommonNumberOfBins(relative[0], relative[1], 1)
+    smoothed = gestures[i].smoothToMostCommonNumberOfBins(relative[0], relative[1], 1)
+    gestures[i].bins_left_filtered, gestures[i].bins_right_filtered = gestures[i].combineNearPeaks(smoothed[0], smoothed[1])
     shifts_left, shifts_right = Feature().featureCountOfShifts(gestures[i])
     featureVector.append(shifts_left + shifts_right)
     featureVector.append(shifts_left)
     featureVector.append(shifts_right)
     
-    featureVector.append(Feature().featureOrderOfShifts(gestures[i], 2))
+    featureVector.extend(Feature().featureOrderOfShifts(gestures[i], 2))
     featureVector.append(Feature().featureAmplitudes(gestures[i]))
     
     distance_contrary, distance_equal = Feature().shiftDistance(gestures[i])
@@ -39,7 +54,12 @@ for i in range(len(gestures)):
 
     
     gestures[i].featureVector = featureVector
-    print featureVector
+    #print featureVector
+    if( featureVector[3] == 0):
+        counter += 1
+
+print 100. / len(gestures) * counter
+    
     
     
 data = []
@@ -52,23 +72,23 @@ for gesture in gestures:
     data2.append(l)
 
 targets = []
-for class_ in [2,3,4,6]:
+for class_ in [2,2,3,3,4,4,0,0,6,6]:
     for frameIndex in range(50):
         targets.append(class_)
+
 
 #X_train, X_test, y_train, y_test = cross_validation.train_test_split(data2, targets, test_size=0.4, random_state=0)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(data, targets, test_size=0.4, random_state=0)
 
 for i in range(1,100):
-    clf = AdaBoostClassifier(n_estimators=i)
-    clf.fit(X_train, y_train)
+    #clf = AdaBoostClassifier(n_estimators=i)
+    #clf.fit(X_train, y_train)
     #result = clf.predict(X_test) == y_test
     #rightPredicts = len([x for x in result if x == True])
     #print i, 100. / len(result) * rightPredicts
     
-    #clf = GradientBoostingClassifier(n_estimators=i, max_depth=2, random_state=0).fit(X_train, y_train)
+    clf = GradientBoostingClassifier(n_estimators=i, max_depth=1, random_state=0).fit(X_train, y_train)
     result = clf.predict(X_test) == y_test
     #result = clf.predict(data[150:]) == targets[150:]
     rightPredicts = len([x for x in result if x == True])
     print i, 100. / len(result) * rightPredicts
-    print clf.predict([0, 0, 0, -1, 0, 0, 0])
--- src/classifier/trees/Feature.py
+++ src/classifier/trees/Feature.py
@@ -13,6 +13,8 @@ class Feature(object):
         cases[1] = [['right','left','right','left'], ['right','left','right']]
         cases[2] = [['both', 'both'], ['both']]
         
+        both_counter = 0
+        
         shifts_left = self.findShifts(gesture.bins_left_filtered, 'left')
         shifts_right = self.findShifts(gesture.bins_right_filtered, 'right')
         gesture.shifts_left = shifts_left
@@ -32,13 +34,15 @@ class Feature(object):
                     diffStart = abs(start_current - start_prev)
                     intersection = abs(max(start_current,start_prev) - min(stop_current,stop_prev))
                     union = abs(min(start_current,start_prev) - max(stop_current,stop_prev))
-                    #print "intersection", intersection
-                    #print "union", union
-                    #print "ffffffffff", union - intersection
                     coverage = union - intersection
-                    if(diffStart < maxDiff or coverage <= 2):
+                    prev_in_curr = start_current <= start_prev and stop_current >= stop_prev
+                    curr_in_prev = start_prev <= start_current and stop_prev >= stop_current
+                    #print prev_in_curr
+                    #print curr_in_prev
+                    if(diffStart < maxDiff or coverage <= 1 or prev_in_curr or curr_in_prev):
                         orderList.pop()
                         orderList.append('both')
+                        both_counter += 1
                     else:
                         orderList.append(shift[0])
                         prev = shift
@@ -52,10 +56,11 @@ class Feature(object):
         #Cases = self.findShiftCases(orderList, 0, Cases)
         #return Cases
         gesture.shift_order = orderList
+        #print orderList, both_counter
         for k,v in cases.iteritems():
             if(orderList in v):
-                return k  
-        return -1 
+                return k, both_counter
+        return -1, both_counter
         #print "no case available", orderList
         # Herausfinden, welche Kombinationen auch noch oft vorkommen pro case oder rauswerfen?
     
@@ -129,7 +134,7 @@ class Feature(object):
     def featureCountOfShifts(self, gesture):
         countOfShiftLeft = len(self.findShifts(gesture.bins_left_filtered, 'left'))
         countOfShiftRight = len(self.findShifts(gesture.bins_right_filtered, 'right'))
-        return countOfShiftRight, countOfShiftLeft
+        return countOfShiftLeft, countOfShiftRight
   
     # Verschiebung ist charakterisiert durch Anfang, Ende, maximale Binanzahl 
     # und ob sie links oder rechts des Peaks ist        
@@ -144,11 +149,11 @@ class Feature(object):
             value = values[i]
             if(value > commonValue and found == False):
                 tempStart = i
-                tempMax = values[i]
+                tempMax = value
                 found = True
             elif(value > commonValue and found == True):
                 if(value > tempMax):
-                    tempMax = values[i]
+                    tempMax = value
             elif(value <= commonValue and found == True):
                 tempStop = i
                 shift = (direction, tempStart, tempStop, tempMax)
