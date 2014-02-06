50ea4c5bfdb7288148e2bdbd8e8e4e715bf21734
dieUhr <Sebastianrieder@gmx.de>
2014-02-06 21:13:59 +0100

    additional trained HMM Model

--- src/classifier/hmm/config/config.py
+++ src/classifier/hmm/config/config.py
@@ -1,12 +1,12 @@
-n_tries = 1
+n_tries = 2
 
 covariance_type=['tied'] #String describing the type of covariance parameters used by the model. Must be one of 'spherical', 'tied', 'diag', 'full'. 
 algorithm=['viterbi']#, 'map']
 n_components=13 # Number of states in the model.
-n_iter=2 # Number of iterations to perform.
+n_iter=5000 # Number of iterations to perform.
 
 covariance_type_gmm='tied'
-n_iter_gmm=2
+n_iter_gmm=5000
 n_components_gmm=4
 n_mix=n_components
 n_init_gmm = 1
